592f42585e15e04f6c69df21fcc39525df079666
Bruxe-Dev <mnibeza23@gmail.com>
2026-02-05 18:28:46 +0200

    finished the function that  detects  a swipe a push and pull patterns

--- gesture_recognizer.py
+++ gesture_recognizer.py
@@ -27,3 +27,56 @@ class GestureRecognizer:
 
         dx = end[0] - start[0]
         dy = end[1] - start[1]
+
+        distance = np.sqrt(dx**2 + dy**2)
+
+        if distance < threshold:
+            return None
+
+        if abs(dx) > abs(dy):
+            detected_direction = 'right' if dx > 0 else 'left'
+        else:
+            detected_direction = 'down' if dy > 0 else 'up'
+
+        if direction == 'any' or direction == detected_direction:
+            if self.gesture_cooldown == 0:
+                self.gesture_cooldown = self.cooldown_frames
+                return detected_direction
+        
+        return None
+
+
+    def detectPush(self, threshold = 150) -> bool:
+
+        if len(self.position_history) < 5:
+            return False
+        
+        start  = self.position_history[0]
+        end = self.position_history[-1]
+
+        dy = end[1] - start[1]
+
+        if dy > threshold and self.gesture_cooldown ==0 :
+            self.gesture_cooldown = self.cooldown_frames
+            return True
+
+        return False
+
+    
+    def detectPull(self ,threshold =150) -> bool:
+
+        if len(self.position_history) < 5:
+            return False
+        
+        start = self.position_history[0]
+        end = self.position_history[-1]
+
+        dx = end[1] - start[1]
+
+        if dx < -threshold and self.gesture_cooldown == 0:
+            self.gesture_cooldown = self.cooldown_frames
+            return True
+
+        return False
+
+    
